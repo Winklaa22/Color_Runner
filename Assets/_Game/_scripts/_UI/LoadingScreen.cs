c1c82865ab3773bc9858c8da9f3848e40b39174f
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2024-02-18 17:46:10 +0100

    skybox assets

--- Assets/_Game/_scripts/_UI/LoadingScreen.cs
+++ Assets/_Game/_scripts/_UI/LoadingScreen.cs
@@ -8,11 +8,13 @@ public class LoadingScreen : MonoBehaviour
 {
     [SerializeField] private float m_duration = 3.0f;
     [SerializeField] private RectTransform m_rectTransform;
+    [SerializeField] private CanvasGroup m_fadeBackground;
     
     private IEnumerator Start()
     {
+        m_fadeBackground.DOFade(0, .5f);
         yield return new WaitForSeconds(m_duration);
-        
-        m_rectTransform.DOMoveY(-Screen.height, 0.3f).OnComplete(()=> { SceneManager.LoadScene("Menu"); });
+        m_fadeBackground.DOFade(1, .5f);
+        m_rectTransform.DOMoveY(-Screen.height, 0.5f).OnComplete(()=> { SceneManager.LoadScene("Menu"); });
     }
 }
