775a9c2b709e2fe0e90a8ea6a3674305cb537ebb
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2024-03-10 17:40:17 +0100

    Curved world fix

--- Assets/_Game/_scripts/_CurvedWorld/CurvedWorldController.cs
+++ Assets/_Game/_scripts/_CurvedWorld/CurvedWorldController.cs
@@ -8,6 +8,12 @@ public class CurvedWorldController : MonoBehaviour
     [SerializeField] private float m_horizontalValue;
     [SerializeField] private float m_verticalValue;
 
+    private void Start()
+    {
+        Shader.SetGlobalFloat("_VerticalCurve", m_verticalValue / 10000);
+        Shader.SetGlobalFloat("_HorizontalCurve", m_horizontalValue / 10000);
+    }
+
     private void Update()
     {
         if (Application.isPlaying)
