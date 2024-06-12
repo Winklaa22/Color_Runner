c0841c8736a45d8584b2f35a361efc2d94e4805d
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2024-06-12 21:08:00 +0200

    Reach meters text

--- Assets/_Game/_scripts/_UI/_Screens/_Views/DeathScreen/DeathScreen.cs
+++ Assets/_Game/_scripts/_UI/_Screens/_Views/DeathScreen/DeathScreen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DeathScreen : View
@@ -7,11 +8,13 @@ public class DeathScreen : View
     [Header("Popups")]
     [SerializeField] private View m_collectCoinsPopup;
     [SerializeField] private View m_choicePopup;
+    [SerializeField] private TMP_Text m_metersCounter;
 
     protected override void OnViewOpened()
     {
         base.OnViewOpened();
-       
+
+        m_metersCounter.text = m_metersCounter.text.Replace("%", ((int) GameManager.Instance.Meters).ToString());
         
         if(GameManager.Instance.Coins > 0)
             m_screenController.OpenPopup(m_collectCoinsPopup);
