a7b2a4922c1690fc442e4a9d56b781ab9f89de09
Winklaa22 <49796412+Winklaa22@users.noreply.github.com>
2024-07-19 17:41:41 +0200

    Safe area controller

---
+++ Assets/_Game/_scripts/_UI/_Safearea/Safearea.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safearea : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        ApplySafeArea();
+    }
+
+    private void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        _rectTransform.anchorMin = anchorMin;
+        _rectTransform.anchorMax = anchorMax;
+    }
+
+    void OnEnable()
+    {
+        ApplySafeArea();
+    }
+}
